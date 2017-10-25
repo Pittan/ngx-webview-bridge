379ad3d0ebba2ee6dce21b39e2ff181f7c2683b9
Amon Keishima <amon.keishima@gmail.com>
2017-10-25 23:26:42 +0900

    chore(all): demo fixed

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  LibModule  } from 'ngx-webview-bridge';
+import {  WebviewBridgeModule  } from 'ngx-webview-bridge';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -8,7 +8,7 @@ import { HomeComponent } from './home.component';
 @NgModule({
     imports: [
         CommonModule,
-        LibModule.forRoot(),
+        WebviewBridgeModule.forRoot(),
         HomeRoutingModule,
     ],
     declarations: [HomeComponent],
